b1bd486b2a14c5b7823225b5beb7bec063624070
xixi <haha@example.com>
2017-07-25 01:05:28 +0800

    首次采用python进行爬虫试验

---
+++ ant/huang/baike_spider/html_downloader.py
@@ -0,0 +1,18 @@
+# coding:utf8
+import urllib2
+
+__author__ = 'ou'
+
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib2.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
